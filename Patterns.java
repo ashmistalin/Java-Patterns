8e3f95f403ac858379edcbb71b45b914fd0ddeba
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-03-14 10:49:36 +0530

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -23,3 +23,12 @@ public class Main {
        pat2.patternsforjava();
     }
 }
+
+
+# OUTPUT:
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=4018:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\Dell\IdeaProjects\java full stack\out\production\java full stack" Main
+ *********
+  *******
+   *****
+    ***
+     *
