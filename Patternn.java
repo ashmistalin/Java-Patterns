360cb37c28df5b8111126c7621bed033f0ad1295
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-03-14 11:02:06 +0530

    Update Patternn.java

--- Patternn.java
+++ Patternn.java
@@ -11,3 +11,23 @@ public class Patternn {
         }
     }
 }
+
+# Main function:
+
+public class Main {
+    public static void main(String[] args) {
+        Patternn pat2=new Patternn();
+       pat2.patts();
+    }
+}
+
+
+# OUTPUT:
+
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=4249:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\Dell\IdeaProjects\java full stack\out\production\java full stack" Main
+*****
+*****
+*****
+*****
+*****
+
