c4e78b6e3f71756b137afbd94ba8a07f632da7c2
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-03-14 10:58:09 +0530

    Add files via upload

---
+++ Programpattern.java
@@ -0,0 +1,22 @@
+public class Programpattern {
+    public void javaprogram(){
+        int i,j;
+        for(i=1;i<=5;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+        for(i=4;i>=1;i--)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+
+}
